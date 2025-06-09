b5a8dc4fbf0b93e40a6f0b7ef0f35f201224158f
Rafael Cuchilla <antonio9550@gmail.com>
2025-06-08 21:09:26 -0600

    leccion 111

---
+++ src/app/country/components/search-input/search-input.component.ts
@@ -0,0 +1,13 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './search-input.component.html',
+})
+export class SearchInputComponent {
+  value = output<string>();
+  placeholder = input<string>('Buscar');
+
+
+}
